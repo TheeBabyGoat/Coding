bd7764e90003459c6e6b339aace16ce15220b75c
TheBabyGoat <whitt_timothy9187@outlook.com>
2025-07-11 15:36:44 -0400

    Added VFX Editor Source Code

---
+++ BabyGoat-Killa's Volumetric Repo/VFX Editor Source Code/editor/src/app/logger.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "helpers/helpers.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+inline spdlog::logger* getLogger()
+{
+	static std::shared_ptr<spdlog::logger> logger = spdlog::basic_logger_mt("log", "__TceLog.log", std::ios_base::trunc);
+	return logger.get();
+}
+
+template<typename T>
+inline void LogInfo(const T& str)
+{
+	auto* logger = getLogger();
+	logger->info(str);
+	logger->flush();
+}
+
+template<typename... T>
+inline void LogInfo(std::format_string<T...> fmt, T&&... args)
+{
+	auto* logger = getLogger();
+	logger->info(vfmt(fmt, std::forward<T>(args)...));
+	logger->flush();
+}
+
+inline void logPtrBytes(u8* address, size_t len)
+{
+	std::string res;
+	res.reserve(len * 3);
+	for (size_t i = 0; i < len; ++i)
+	{
+		res += vfmt("{:02X} ", address[i]);
+	}
+	LogInfo(res);
+}
+
