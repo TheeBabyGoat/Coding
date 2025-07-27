3298085c10bd0549394a6598127da738fb789358
TheBabyGoat <whitt_timothy9187@outlook.com>
2025-07-27 18:32:38 -0400

    Delete AI Feeder/beamNG Volume Clouds directory

--- AI Feeder/beamNG Volume Clouds/lua/ge/client/volumetricPrecipitation.lua
+++
@@ -1,9 +0,0 @@
-log('D', "volumetricPrecipitation", "Creating Volumetric Precipitation")
-
-local VPShaderData = createObject("ShaderData")
-VPShaderData.DXVertexShaderFile = "shaders/common/particles/volumetricPrecipitationV.hlsl"
-VPShaderData.DXPixelShaderFile  = "shaders/common/particles/volumetricPrecipitationP.hlsl"
-VPShaderData.pixVersion = 5.0;
-VPShaderData:registerObject("VolumetricPrecipitationShaderData")
-
--- local shaders = scenetree.findClassObjects('ShaderData') for shaderIndex = 1, #shaders do print(shaders[shaderIndex]) end
--- AI Feeder/beamNG Volume Clouds/scripts/ChipVolumetricClouds/modScript.lua
+++
@@ -1,2 +0,0 @@
-load("chipVolumetricUtil")
-setExtensionUnloadMode("chipVolumetricUtil", "manual")
