70b47ad8e497530c1b06c3033fb1320e9ee6310a
TheBabyGoat <whitt_timothy9187@outlook.com>
2025-08-20 14:39:02 -0400

    Delete AI Feeder/PulseV Data/PulseV-ASI directory

--- AI Feeder/PulseV Data/PulseV-ASI/config.hpp
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-#include <string>
-
-struct Config {
-    bool verbose = false;
-    bool overlay = false;
-    bool load(const std::string &path);
-};
--- AI Feeder/PulseV Data/PulseV-ASI/logging.hpp
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-#include <string>
-
-void log_set_verbose(bool enabled);
-void log_info(const std::string &msg);
-void log_error(const std::string &msg);
-void log_debug(const std::string &msg);
