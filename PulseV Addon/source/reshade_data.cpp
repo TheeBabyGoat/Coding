c06731d896a8cd61341d9c6ee6056256a8819d09
TheBabyGoat <whitt_timothy9187@outlook.com>
2025-12-24 07:51:13 -0500

    Added PulseV Reshade Addon

---
+++ PulseV Addon/source/reshade_data.cpp
@@ -0,0 +1,50 @@
+#include "reshade_data.hpp"
+
+static UInt2    render_target_resolution = { 0, 0 };
+static uint32_t render_target_width = 0;
+static uint32_t render_target_height = 0;
+static void* first_swapchain_hwnd = NULL;
+
+void on_init_swapchain(reshade::api::swapchain* swapchain, bool resize)
+{
+    void* hwnd = swapchain->get_hwnd();
+
+    if (first_swapchain_hwnd == NULL) {
+        first_swapchain_hwnd = hwnd;
+    }
+
+    if (hwnd != first_swapchain_hwnd) {
+        return;
+    }
+
+    const auto& back_buffer =
+        swapchain->get_device()->get_resource_desc(swapchain->get_current_back_buffer());
+
+    if (static_cast<int>(back_buffer.usage & reshade::api::resource_usage::render_target) == 0) {
+        return;
+    }
+
+    LOG(("Detected resolution: " + std::to_string(back_buffer.texture.width) +
+        "x" + std::to_string(back_buffer.texture.height)).c_str());
+
+    render_target_width = back_buffer.texture.width;
+    render_target_height = back_buffer.texture.height;
+
+    render_target_resolution = {
+        render_target_width,
+        render_target_height
+    };
+}
+
+const UInt2 get_render_target_resolution() {
+    return render_target_resolution;
+}
+
+uint32_t get_render_target_width() {
+    return render_target_width;
+}
+
+uint32_t get_render_target_height() {
+    return render_target_height;
+}
+
